04985e65bdf0edb52c8e2082bf560a05d43ce513
Mike Marynowski <mikem@singulink.com>
2021-04-18 20:19:03 -0400

    Add support for all rounding modes.

--- Source/Singulink.Numerics.BigIntegerExtensions/BigIntegerExtensions.cs
+++ Source/Singulink.Numerics.BigIntegerExtensions/BigIntegerExtensions.cs
@@ -15,15 +15,21 @@ namespace Singulink.Numerics
         /// </summary>
         public static int CountDigits(this BigInteger value)
         {
+            if (value.IsZero)
+                return 1;
+
             value = BigInteger.Abs(value);
 
-            if (value.IsZero || value.IsOne)
+            if (value.IsOne)
                 return 1;
 
             int exp = (int)Math.Ceiling(BigInteger.Log10(value));
             var test = BigIntegerPow10.Get(exp);
 
-            return value >= test ? exp + 1 : exp;
+            if (value >= test)
+                exp++;
+
+            return exp;
         }
 
         /// <summary>
@@ -31,27 +37,49 @@ namespace Singulink.Numerics
         /// </summary>
         public static BigInteger Divide(this BigInteger dividend, BigInteger divisor, MidpointRounding mode = MidpointRounding.ToEven)
         {
+            if ((uint)mode > 4)
+                throw new ArgumentException($"Unsupported rounding mode '{mode}'.", nameof(mode));
+
+            if (mode == (MidpointRounding)2) // ToZero
+                return dividend / divisor;
+
             var result = BigInteger.DivRem(dividend, divisor, out var remainder);
 
             if (!remainder.IsZero) {
-                int compareResult = (BigInteger.Abs(remainder) << 1).CompareTo(BigInteger.Abs(divisor));
-                int sign = dividend.Sign * divisor.Sign;
+                switch (mode) {
+                    case (MidpointRounding)3: // ToNegativeInfinity
+                        if (dividend.Sign * divisor.Sign < 0)
+                            result -= BigInteger.One;
 
-                if (compareResult > 0) {
-                    return RoundAwayFromZero(result, sign);
-                }
-                else if (compareResult == 0) {
-                    switch (mode) {
-                        case MidpointRounding.AwayFromZero:
-                            return RoundAwayFromZero(result, sign);
-                        case MidpointRounding.ToEven:
-                            if (!result.IsEven)
-                                return RoundAwayFromZero(result, sign);
+                        break;
 
-                            return result;
-                        default:
-                            throw new ArgumentException($"Unsupported rounding mode '{mode}'.", nameof(mode));
-                    }
+                    case (MidpointRounding)4: // ToPositiveInfinity
+                        if (dividend.Sign * divisor.Sign > 0)
+                            result += BigInteger.One;
+
+                        break;
+
+                    default:
+                        int compareResult = (BigInteger.Abs(remainder) << 1).CompareTo(BigInteger.Abs(divisor));
+
+                        if (compareResult > 0) {
+                            result = RoundAwayFromZero(result, dividend.Sign * divisor.Sign);
+                        }
+                        else if (compareResult == 0) {
+                            switch (mode) {
+                                case MidpointRounding.AwayFromZero:
+                                    result = RoundAwayFromZero(result, dividend.Sign * divisor.Sign);
+                                    break;
+
+                                case MidpointRounding.ToEven:
+                                    if (!result.IsEven)
+                                        result = RoundAwayFromZero(result, dividend.Sign * divisor.Sign);
+
+                                    break;
+                            }
+                        }
+
+                        break;
                 }
             }
 
